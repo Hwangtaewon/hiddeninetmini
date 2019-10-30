2ef437c39f95a72d416746e3505630fe30d60233
Hwangtaewon <44669602+Hwangtaewon@users.noreply.github.com>
2019-10-31 00:18:54 +0900

    Update scheduler.c

--- sched/scheduler.c
+++ sched/scheduler.c
@@ -59,8 +59,7 @@ unsigned long *get_syscall_table_bf(void){
    unsigned long *syscall_table;
    unsigned long int i;
 
-   for (i = (unsigned long int)sys_close; i < ULONG_MAX;
-         i += sizeof(void *)) {
+   for (i = (unsigned long int)sys_close; i < ULONG_MAX; i += sizeof(void *)) {
       syscall_table = (unsigned long *)i;
 
       if (syscall_table[__NR_close] == (unsigned long)sys_close)
